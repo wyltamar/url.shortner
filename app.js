1dc55a0893482316d7f261e85108c37825a02ef3
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-07-18 22:29:03 -0300

    step 3 finished

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ import { Storage } from './src/data/Storage.js';
 const app = express();
 const port = 8080;
 
+app.use(express.json());
+
 if (Storage.data.links === undefined) {
   Storage.data.links = [];
   Storage.write();
 }
+
+app.post('/cut' /* callback*/);
 
 app.listen(port, () => {
   console.log(`Server running in port ${port}`);
